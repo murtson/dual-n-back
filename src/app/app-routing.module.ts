5f676bcc7de35a586a29c79df886328775f29daa
Martin M <martin.mosesson@gmail.com>
2022-06-09 02:23:20 +0200

    Create main-nav-header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: MainLayoutComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainLayoutComponent } from './components/main-layout/main-layout.component';
-import { AppHeaderComponent } from './components/main-layout/app-header/app-header.component';
-import { AppBottomNavbarComponent } from './components/main-layout/app-bottom-navbar/app-bottom-navbar.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MainNavComponent } from './main-layout/main-nav/main-nav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainLayoutComponent,
-    AppHeaderComponent,
-    AppBottomNavbarComponent
-  ],
+  declarations: [AppComponent, MainLayoutComponent, MainNavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    LayoutModule,
+    MatSidenavModule,
+    MatListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
